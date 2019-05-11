b98fdd35177591802c53ec89b85d13c15788c998
Christian Caicedo <ccaicedo@credera.com>
2019-05-10 23:06:28 -0500

    made client, but wont work.

--- sketches/wifi_nodeMCU_main.ino/wifi_nodeMCU_main.ino.ino
+++ sketches/wifi_nodeMCU_main.ino/wifi_nodeMCU_main.ino.ino
@@ -1,21 +1,22 @@
 #include <ESP8266WiFi.h>
-#include <WiFiClient.h>
-#include <ESP8266WebServer.h>
 
 // Replace with your network credentials
-const char* ssid = "<YOUR WIFI SSID>";
-const char* password = "<YOUR WIFI PASSWORD>";
+const char* ssid = "ASUSHub24_2GEXT";
+const char* password = "4thDimension";
 
-ESP8266WebServer server(80);   //instantiate server at port 80 (http port)
+const char* resource = "/workflows/3495f95b917a42e9a39e7959423e28dd/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=nCoh7VcTBQrJ8ncKPKky5DupaQmB8d5zkuLhZmNCKe8";
+const char* host = "prod-09.centralus.logic.azure.com";
+int Sample;
 
-String page = "";
-int LEDPin = 13;
+WiFiClient client;
+
+int sensorDigitalPin = D8;
+int sensorAnalogPin = A0;
+
 void setup(void){
-  //the HTML of the web page
-  page = "<h1>Simple NodeMCU Web Server</h1><p><a href=\"LEDOn\"><button>ON</button></a>&nbsp;<a href=\"LEDOff\"><button>OFF</button></a></p>";
-  //make the LED pin output and initially turned off
-  pinMode(LEDPin, OUTPUT);
-  digitalWrite(LEDPin, LOW);
+
+  pinMode(sensorDigitalPin, INPUT);
+  pinMode(sensorAnalogPin, INPUT);
    
   delay(1000);
   Serial.begin(115200);
@@ -32,24 +33,39 @@ void setup(void){
   Serial.println(ssid);
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
-   
-  server.on("/", [](){
-    server.send(200, "text/html", page);
-  });
-  server.on("/LEDOn", [](){
-    server.send(200, "text/html", page);
-    digitalWrite(LEDPin, HIGH);
-    delay(1000);
-  });
-  server.on("/LEDOff", [](){
-    server.send(200, "text/html", page);
-    digitalWrite(LEDPin, LOW);
-    delay(1000); 
-  });
-  server.begin();
-  Serial.println("Web server started!");
 }
  
 void loop(void){
-  server.handleClient();
+  int analogValue = analogRead(sensorAnalogPin);
+  Serial.println(digitalRead(sensorDigitalPin));
+  Serial.println(analogValue);
+  delay(1000);
+  if (client.connect(host,443))
+  { 
+    String postStr = "";
+    postStr +="{\"mydata\":" + String(analogValue) + "\"}";
+
+    Serial.println(postStr);
+
+    client.print("POST ");
+    client.print(resource);
+    client.print(" HTTP/1.1\n");
+    client.print("Host:");
+    client.print(host); client.print("\n");
+    client.print("Connection: close\n");
+    client.print("Content-Type: application/json\n");
+    client.print("Content-Length: ");
+    client.print(postStr.length());
+    client.print("\n\n");
+    client.print(postStr);
+
+    while(client.available()){
+      char c = client.read();
+      Serial.print(c);
+    }
+    Serial.println("");
+  }
+  client.stop();
+  Serial.println("Waiting…");
+  delay(10000);
 }
